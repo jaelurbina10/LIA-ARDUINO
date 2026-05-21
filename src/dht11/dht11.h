60b26f3b8196193e9cf5aac3e148ac5755471bc9
theBaltman04 <bzamudio208@uvaq.edu.mx>
2026-05-20 22:44:21 -0600

    dht

---
+++ src/dht11/dht11.h
@@ -0,0 +1,38 @@
+#ifndef DHT11_H
+#define DHT11_H
+
+#include "sensor/sensor.h"
+#include <DHT.h>
+
+class dht11 : public Sensor {
+private:
+    uint8_t _pin;
+    DHT _dht; 
+    
+    float _temperatura;
+    float _humedad;
+
+    long _ultimoTLectura;
+    const long _interLectura = 1000;
+
+    const float _umbralTempAlta = 37.0;
+    const float _umbralTempBaja = 15.0;
+    const float _umbralHumAlta = 80.0;
+    const float _umbralHumBaja = 40.0;
+
+public:
+    dht11(uint8_t p);
+
+    void begin() override;
+    void process() override;
+
+    bool muchoCalor() const;
+    bool muchoFrio() const;
+    bool muchaHumedad() const;
+    bool pocaHumedad() const;
+
+    float getTemperatura() const;
+    float getHumedad() const;
+};
+
+#endif
