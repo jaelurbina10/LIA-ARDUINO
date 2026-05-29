b7b7c4e16398c4386b2612da661f59ca7f992e8b
theBaltman04 <bzamudio208@uvaq.edu.mx>
2026-05-29 13:27:41 -0600

    controller

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -1,24 +1,40 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
-#include <stdint.h>
+
+#include <Arduino.h>
 #include "sensor.h"
 #include "actuador.h"
 
-namespace LIA{
-    class Controller{
-        private :
-            uint8_t _NoSensores;
-            uint8_t _NoActuadores;
-            sensor** _sensores;
-            actuador** _actuadores;
+namespace LIA {
 
-        public:
-            Controller(uint8_t, uint8_t){};
+class Controller {
+private:
+    uint8_t _maxSensores;
+    uint8_t _maxActuadores;
+    
+    uint8_t _sensorCount;
+    uint8_t _actuadorCount;
 
-            bool addSensor(sensor&, uint8_t){};
+    Sensor** _sensores;
+    Actuador** _actuadores;
 
-    }
+public:
+    Controller(uint8_t maxSensores, uint8_t maxActuadores);
+
+    bool addSensor(Sensor* s);
+    bool addActuador(Actuador* a);
+
+    virtual void begin() = 0;
+    virtual void process() = 0;
+
+protected:
+    Sensor* getSensor(uint8_t index) const;
+    Actuador* getActuador(uint8_t index) const;
+    
+    uint8_t getSensorCount() const;
+    uint8_t getActuadorCount() const;
+};
+
 }
-
 
 #endif
