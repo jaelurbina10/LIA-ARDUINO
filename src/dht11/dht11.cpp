00c9ae821f801202ed91e22ab33fe41958cd9846
BALTMAN <bzamudio208@uvaq.edu.mx>
2026-06-06 15:01:08 -0600

    dht11 corection

--- src/dht11/dht11.cpp
+++ src/dht11/dht11.cpp
@@ -1,44 +1,54 @@
 #include "dht11.h"
 
-dht11::dht11(uint8_t p) : _pin(p), _dht(p, DHT11), _temperatura(0.0), _humedad(0.0), _ultimoTLectura(0) {}
-
-void dht11::begin() {
-    _dht.begin();
+dht11::dht11(uint8_t p) : _pin(p), _dht11(_pin){
+    _startTime = 0;
+    _periodMs = 2000;
+    _highTemp = 37.0;
+    _lowTemp = 15.0;
+    _highHum = 80.0;
+    _lowHum = 40.0;
 }
 
+void dht11::begin() {}
+
 void dht11::process() {
-    unsigned long tiempoActual = millis();
-
-    if (tiempoActual - _ultimoTLectura >= _interLectura) {
-        
-        _ultimoTLectura = tiempoActual;
-
-        float t = _dht.readTemperature();
-        float h = _dht.readHumidity();
-
-        if (!isnan(t)) {
-            _temperatura = t;
-        }
-        if (!isnan(h)) {
-            _humedad = h;
+    if(millis() - _startTime >= _periodMs) {
+        _startTime = millis();
+        float a = 0.0, b = 0.0;
+        _dhtReading = _dht11.readTemperatureHumidity(a,b);
+        if(_dhtReading == 0) {
+            _temperatura = a;
+            _humedad = b;
+        }else{
+            Serial.print(millis());
+            Serial.print(" --> DHT11 Error: ");
+            Serial.println(_dhtReading);
         }
     }
 }
 
+void dht11::setThresholds(float highTemp, float lowTemp, float highHum, float lowHum) {
+    _highTemp = highTemp;
+    _lowTemp = lowTemp;
+    _highHum = highHum;
+    _lowHum = lowHum;
+}
+
+
 bool dht11::haceCalor() const {
-    return _temperatura >= _umbralTempAlta;
+    return _temperatura >= _highTemp;
 }
 
 bool dht11::haceFrio() const {
-    return _temperatura <= _umbralTempBaja;
+    return _temperatura <= _lowTemp;
 }
 
 bool dht11::muchaHumedad() const {
-    return _humedad >= _umbralHumAlta;
+    return _humedad >= _highHum;
 }
 
 bool dht11::pocaHumedad() const {
-    return _humedad <= _umbralHumBaja;
+    return _humedad <= _lowHum;
 }
 
 float dht11::getTemperatura() const {
--- src/dht11/dht11.h
+++ src/dht11/dht11.h
@@ -2,29 +2,32 @@
 #define DHT11_H
 
 #include "sensor/sensor.h"
-#include <DHT.h>
+#include <DHT11.h
 
 class dht11 : public Sensor {
 private:
     uint8_t _pin;
-    DHT _dht; 
+    DHT11 _dht11;
+    int _dhtReading;
     
     float _temperatura;
     float _humedad;
 
-    unsigned long _ultimoTLectura;
-    const unsigned long _interLectura = 1000;
+    float _startTime;
+    uint32_t _periodMs;
 
-    const float _umbralTempAlta = 37.0;
-    const float _umbralTempBaja = 15.0;
-    const float _umbralHumAlta = 80.0;
-    const float _umbralHumBaja = 40.0;
+    float _highTemp;
+    float _lowTemp;
+    float _highHum;
+    float _lowHum;
 
 public:
     dht11(uint8_t p);
 
     void begin() override;
     void process() override;
+
+    void setThresholds(float highTemp, float lowTemp, float highHum, float lowHum);
 
     bool haceCalor() const;
     bool haceFrio() const;
